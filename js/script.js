afcd8348a5ef2c701cc6a7b0226a29e28c7145e3
juandaniel <jddj1897@gmail.com>
2022-07-17 20:11:20 -0500

    delay change

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const sr = ScrollReveal ({
     reset: true
 });
 
-sr.reveal('.home-text', {delay:350, origin:'left'});
-sr.reveal('.home-img', {delay:350, origin:'right'});
+sr.reveal('.home-text', {delay:10, origin:'left'});
+sr.reveal('.home-img', {delay:10, origin:'right'});
 
-sr.reveal('.sub-service, .about, .portfolio, .service, .contact', {delay:200, origin:'bottom'});
+sr.reveal('.sub-service, .about, .portfolio, .service, .contact', {delay:10, origin:'bottom'});
